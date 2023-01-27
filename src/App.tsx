0df6753a1b46f2e4463d86130899f4c99bb44dd6
bodyarty <studentartb@gmail.com>
2023-01-27 15:41:08 +0300

    Main screen ready

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,22 @@ function App() {
 
     return (
         <div className="app content-container">
-            <h1 className="title-accent">Forbidden Island</h1>
+            <div className="app-header">
+                <h1 className="title-accent">
+                    Тест
+                    <br />
+                    «НЕОБИТАЕМЫЙ ОСТРОВ»
+                </h1>
+                <div className="inscription">
+                    <div className="inscription-content">
+                        Определи свой психотип и узнай сильные стороны своей
+                        натуры для профориентации и достижения успеха
+                    </div>
+                </div>
+            </div>
+            <div className="app-footer">
+                <button className="btn btn-primary">Далее</button>
+            </div>
         </div>
     );
 }
